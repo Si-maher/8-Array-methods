5908ec0245550e81d2a7851b319f599354925d10
Simon Maher <simonmaher8307@hotmail.com>
2021-03-03 13:15:04 +0200

    added filter method

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const items =[
-    {
-        name:'Bike', price: 100,
-        name:'Tv', price: 200,
-        name:'Album', price: 10,
-        name:'Book', price: 5,
-        name:'Phone', price: 500,
-        name:'Computer', price: 1000,
-        name:'Keyboard', price: 25
-    }
+
+    {name:'Bike', price: 100},
+        {name:'Tv', price: 200},
+        {name:'Album', price: 10},
+        {name:'Book', price: 5},
+        {name:'Phone', price: 500},
+        {name:'Computer', price: 1000},
+        {name:'Keyboard', price: 25}
 ]
+
+// 1) Filter 
+
+const filteredItems = items.filter((item) => {
+    return item.price <= 100
+})
+console.log(items);
+console.log(filteredItems);
+
+// Filter method does not alter the original array 
